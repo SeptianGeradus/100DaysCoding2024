f838537123b1dfd6cc35c5a493223c528d51b334
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-05 18:57:49 +0800

    Hari ke empat

---
+++ Day4.java
@@ -0,0 +1,5 @@
+public class Day4 {
+    public static void main(String[] args){
+        System.out.printf("Nama = %s, Nim = %s, Umur =%d, Tinggi Badan = %.1f%n", "Septian Geradus", "D0224327", 17 , 159.4);
+    }
+}
