867b0e9500d7dae1e2b5ad82e4cae89bb2d3cacd
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-09 18:42:07 +0800

    Hari Ke Delapan

---
+++ Day8.java
@@ -0,0 +1,8 @@
+public class Day8 {
+	public static void main(String[] args) {
+		String nama = "Septian Geradus ";
+		int usia = 17;
+		System.out.println("Nama:" + nama);
+		System.out.println("Usia:" + usia);
+	   }
+ }
