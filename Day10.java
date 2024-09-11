98dc758140c0ccff6515f18cb77137b877b44166
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-11 21:43:01 +0800

    Hari Ke Sepuluh

---
+++ Day10.java
@@ -0,0 +1,12 @@
+package TypeDataByte;
+public class Day10 {
+	public static void main(String[] args) {
+		byte a= -128;
+		byte b = 127;
+	// angka dari -128 sampai 127
+		System.out.println (a);
+		System.out.println(b);
+		
+	
+	}
+}
