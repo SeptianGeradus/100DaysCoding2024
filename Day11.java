66f3e1fe03b7dfcdc0555fea4974bd0c65292db6
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-12 16:20:32 +0800

    Hari Ke Sebelas

---
+++ Day11.java
@@ -0,0 +1,12 @@
+package TypeDataShort;
+public class Day11 {
+	public static void main(String[] args) {
+		short a;
+		short b;
+		a= -32768;
+		b= 32767;
+	// angka dari -32768 sampai 32767
+		System.out.println (a);
+		System.out.println(b);
+			}
+		}
