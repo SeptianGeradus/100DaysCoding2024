af2b10bd3b60673103a36ec370b2deddded428ed
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-13 21:40:54 +0800

    Hari Ke 12

---
+++ Day12.java
@@ -0,0 +1,11 @@
+package TypeDataInt;
+
+public class Day12 {
+	public static void main(String[] args){ 
+		int umur = 17;
+	// jangkauan	-2147483648 sampai 2147483647
+	
+	System.out.println(umur);
+		
+    	}
+    }
