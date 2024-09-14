80f9a8dfb10ed5dcffecc8d91f400ba4f8f1af44
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-14 10:57:03 +0800

    Hari ke Tiga Belas

---
+++ Day13.java
@@ -0,0 +1,13 @@
+package Evaluasi;
+
+public class Day13 {
+    
+    public static void main(String[] args) {
+       byte C;
+            C =8;
+             System.out.println(C);
+              C=9;
+             System.out.println(C);
+    }
+    
+}
