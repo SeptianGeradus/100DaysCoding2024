d27b4927cea1c78ed6aaaaebc792eaac77a170f4
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-20 21:23:54 +0800

    Hari ke sembilan belas

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class Day19 {
+    public static void main(String[] args) {
+        // mendeklarasikan dan inisialisasikan String
+        String nama = " cewek";
+        
+              // Menggabungkan String
+        String katakan = "kiw kiw , " + nama + ":)";
+        System.out.println(katakan);
+        
+    }
+ }
