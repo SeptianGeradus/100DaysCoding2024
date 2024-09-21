67c1368a5d80082496c85f781ee641536140345d
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-21 20:06:46 +0800

    Hari ke Dua Puluh

---
+++ Day20.java
@@ -0,0 +1,10 @@
+public class Day20 {
+    public static void main(String[] args) {
+       
+        byte umur1 = 18; //  tahun
+        byte umur2 = 19; //  tahun
+
+        System.out.println("Umur 1: " + umur1 + " tahun");
+        System.out.println("Umur 2: " + umur2 + " tahun");
+    }
+}
