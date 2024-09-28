b8ecf10ace88c25626ee4624824db3175b5376f0
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-28 20:01:51 +0800

    Hari ke dua puluh tujuh soal evaluasi pertama

---
+++ Day27.java
@@ -0,0 +1,17 @@
+public class PengelolaanTanaman {
+   static int jumlahTanaman = 100;
+   
+       public static void main(String[] args) {
+        System.out.println("Selamat datang di pengelolaan tanaman!");
+      
+          String namaTanaman = "Melati";
+
+    // Variabel konstanta tinggi tanaman
+   final int tinggi_Tanaman= 50;
+
+    // Variabel untuk updatenya 
+        int tinggiTanaman = 140;
+ 
+         System.out.printf("Tanaman bernama %s memiliki tinggi %d cm. Jumlah stok tanaman saat ini adalah %d.\n", namaTanaman, tinggiTanaman, jumlahTanaman);
+         }
+    }
