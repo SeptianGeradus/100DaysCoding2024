a0ba463faebdd8a322eecf6a260b7ccdabbd33db
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-29 21:59:04 +0800

    Hari ke dua puluh delapan

---
+++ Day28.java
@@ -0,0 +1,9 @@
+public class Day28 {
+    public static void main(String[] args) {
+        Boolean makan_nasi  = false ; // true
+       
+  System.out.print("Tian Suka Makan Nasi  : "+ makan_nasi); 
+ 
+       }
+    
+}
