4368810a956398ee7cc08b9a9054af3d9531d156
SeptianGeradus <septiangeradus322@gmail.com>
2024-09-30 20:33:31 +0800

    Hari ke duapuluh sembilan

---
+++ Day29.java
@@ -0,0 +1,10 @@
+import java.math.BigInteger;
+
+public class Day29 {
+    
+    public static void main(String[] args) {
+        BigInteger a = new BigInteger("21314142555");
+        System.out.print("BigInteger: " + a);
+    }
+    
+}
