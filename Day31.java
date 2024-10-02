9d8138debd2d4aeedc88388eeca7c360c7c8c2d6
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-02 21:19:00 +0800

    Hari ke tigapuluh satu

---
+++ Day31.java
@@ -0,0 +1,10 @@
+public class Day31 {
+	public static void main(String[] args) {
+		//konversi String ke tipe data primitif
+	String a = "123"; 
+	
+	// mengubah String ke int
+	int angka =Integer.parseInt(a);
+	System.out.println("String ke int: " + angka);
+		}
+}
