9109e23665696d5083474aeff3317b348e782d9a
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-08 23:47:13 +0800

    Hari ke tigapuluh tujuh

---
+++ Day37.java
@@ -0,0 +1,18 @@
+public class OperatorPenugasan {
+    public static void main(String[] args) {
+        // mengisi nilai
+        int a = 2;
+        System.out.println("Nilai a: " + a);
+        
+        // mengisi dan menambahkan
+        int b = 10;
+        b += 2;  // b = b + 2
+        System.out.println("Nilai b setelah ditambah : " + b);
+        
+        // mengisi dan mengurangi 
+        int c = 20;
+        c -= 7;  // c = c - 7
+        System.out.println("Nilai c setelah dikurang : " + c);
+        
+    }
+}
