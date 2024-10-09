16423defd36d217e880a9416d9851ea9ac663e61
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-09 19:52:54 +0800

    Hari ke tiga puluh delapan

---
+++ Day38.java
@@ -0,0 +1,10 @@
+public class Main {
+	public static void main(String[] args) {
+		int x = 8;
+		x *=2;
+		System.out.println("hasil operator penugasan kali: " + x);
+		x /=4;
+    	System.out.println("hasil operator penugasan bagi: " + x);
+ 
+	}
+}
