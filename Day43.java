70f0262e9a3a6ad6ce0fa250018912f4fd1c3ccd
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-14 21:05:12 +0800

    Hari ke empat puluh tiga

---
+++ Day43.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a = 123;
+		int b = 456;
+		
+		System.out.println(10 >= b);
+		System.out.println(134 <= a);
+		System.out.println(123 <=a);
+		System.out.println(456 >= a);
+	 }
+}
