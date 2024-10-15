2016224a945abfc02e439c2c2edeb784c77883ae
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-16 05:36:42 +0800

    Hari ke empat puluh lima

---
+++ Day45.java
@@ -0,0 +1,11 @@
+public class main {
+    public static void main(String[] args) {
+        int angka = 15;
+
+      if (angka < 10 || angka > 20) {
+            System.out.println("Angka berada di luar rentang 10 hingga 20.");
+        } else {
+            System.out.println("Angka berada dalam rentang 10 hingga 20.");
+        }
+    }
+}
