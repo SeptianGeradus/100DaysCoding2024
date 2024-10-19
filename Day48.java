31cc51bda36f8c29e2e91b3b28080276685a4ac3
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-19 20:45:55 +0800

    Hari ke empat puluh delapan

---
+++ Day48.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		int a=6;
+		int b=7;
+		if (a<b){
+			System.out.println("oke");
+		}else {
+			System.out.println("coba lagi");
+		}
+	}
+}
