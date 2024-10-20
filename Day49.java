8974d62375bd8cb40d747ef6d097aab9fe111cb7
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-20 15:15:58 +0800

    Hari ke empat puluh sembilan

---
+++ Day49.java
@@ -0,0 +1,12 @@
+public class Main {
+	public static void main(String[] args) {
+		int a =8;
+		int b= 9;
+		
+		if (a<b){
+			System.out.println("Mantap");
+		}else if (b>a){
+			System.out.println("oke");
+		}
+	}
+}
