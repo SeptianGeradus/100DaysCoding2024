1d5eadecd8dca2cbddcab99d56e7521ad784c861
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-21 20:25:50 +0800

    Hari ke lima puluh

---
+++ Day50.java
@@ -0,0 +1,14 @@
+public class Main {
+	public static void main(String[] args) {
+	 int a =5;
+	 int b =10;
+	 
+	 if (a+b==15){
+	 	System.out.println("oje");
+	 }	else if (a*b==50){
+	 	System.out.println("oke");
+	 }else {
+	 	System.out.println("nt");
+	 }
+	}
+}
