48ceaa08980da6c2f05aa3acf0cf4c22f00254e5
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-23 18:59:40 +0800

    Hari ke lima puluh dua

---
+++ Day52.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+
+        int max = (a > b) ? a : b;
+        System.out.println("Nilai maksimum adalah: " + max);
+    }
+}
