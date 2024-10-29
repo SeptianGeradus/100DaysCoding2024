ca9dc42eac937718bf0982d1e25c5849e5811137
SeptianGeradus <septiangeradus322@gmail.com>
2024-10-29 22:00:14 +0800

    Hari ke lima puluh delapan

---
+++ Day58.java
@@ -0,0 +1,14 @@
+public class Main{
+    
+    public static void main(String[] args) {
+        
+        int i =20;
+        
+        while (i>2){
+            System.out.println(i);
+            i-=2;
+        }
+        
+    }
+    
+}
