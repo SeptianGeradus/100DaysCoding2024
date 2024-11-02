4a2d15d3bff2ca600483258f5ace8f9cf9346515
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-02 22:27:42 +0800

    Hari ke enam puluh dua

---
+++ Day62.java
@@ -0,0 +1,12 @@
+public class main {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            if (i == 5) {
+                System.out.println(" loop berhenti di angka: " + i);
+                break;
+            }
+            System.out.println("Angka: " + i);
+        }
+        
+    }
+}
