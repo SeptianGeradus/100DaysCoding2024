e72e7302124330e56fd91e48d75de9c3ad02c746
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-07 23:09:50 +0800

    Hari ke enam puluh tujuh

---
+++ Day67.java
@@ -0,0 +1,12 @@
+class gabut{
+int jarak = 161 ;
+
+public static void main (String [] args ) {
+gabut asep = new gabut ();
+
+asep.berjalan();
+}
+void berjalan() {
+System.out.print ("asep berjalan sejauh  "  +  jarak  +  "meter" );
+}
+}
