8294418caf3506eef56595687599cc7b9a99a0a2
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-08 23:11:51 +0800

    Hari ke enam puluh delapan

---
+++ Day68.java
@@ -0,0 +1,13 @@
+public class lagi {
+
+    
+    public int tambah(int a, int b) {
+        return a + b;
+    }
+
+    public static void main(String[] args) {
+        lagi c = new lagi();
+        int hasil = c.tambah(5, 3);
+        System.out.println("Hasil: " + hasil);
+    }
+}
