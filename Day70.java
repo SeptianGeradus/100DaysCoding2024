cfc01d455cb2019599a2fcbf0a6265283c5c0f38
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-10 23:36:47 +0800

    Hari ke tujuh puluh

---
+++ Day70.java
@@ -0,0 +1,8 @@
+public class Main {
+   public static String saya (String nama ,int            umur) {
+       return nama  +  "berumur"  +  umur  + "th";
+     }
+      public static void main (String [] args ) {
+            System.out.println (saya ( "asep" ,       18 )  );
+                   }
+      }
