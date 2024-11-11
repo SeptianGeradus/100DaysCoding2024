dd29a167fee53f92fac5675a26c27bbbeb4789cb
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-11 22:46:40 +0800

    Hari ke tujuh puluh satu

---
+++ Day71.java
@@ -0,0 +1,11 @@
+public class belajar {
+
+  public static void a(String nama) {
+        System.out.println("Halo, " + nama + "! nama saya septian.");
+    }
+
+    public static void main(String[] args) {
+    
+        a("temang");
+    }
+}
