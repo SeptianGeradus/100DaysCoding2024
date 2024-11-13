28eea27afa234df15ae4f9417f4c7cafe7f902b5
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-13 22:54:26 +0800

    Hari ke tujuh puluh tiga

---
+++ Day73.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String name = "septian";
+        for (int i = 0; i < name.length(); i++) {
+            char character = name.charAt(i);
+            System.out.println("Karakter pada indeks " + i + " adalah: " + character);
+        }
+    }
+}
