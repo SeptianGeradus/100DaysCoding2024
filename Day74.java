15353b70c087d283f638ccc18df9a2eec30e5356
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-14 21:52:55 +0800

    Hari ke tujuh puluh empat

---
+++ Day74.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+      
+        String salam = "kiw kiw !";
+        
+        int panjang = salam.length();
+        
+        System.out.println("Panjang string: " + panjang);
+    }
+}
