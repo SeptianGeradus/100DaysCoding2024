c6bb5fe6a05d0900a7e814996680fc5b921fe6e3
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-16 23:02:35 +0800

    Hari ke tujuhpuluh enam

---
+++ Day76.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        String teks = "halo, apa kabar?";
+        
+        // Menggunakan metode toUpperCase() untuk mengubah teks menjadi huruf besar
+        String teksUpperCase = teks.toUpperCase();
+        
+        System.out.println("Teks Asli: " + teks);
+        System.out.println("Teks Huruf Besar: " + teksUpperCase);
+    }
+}
