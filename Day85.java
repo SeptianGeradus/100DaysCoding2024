98539204d2afb197686a7bb4f6a557d514a11f2a
SeptianGeradus <septiangeradus322@gmail.com>
2024-11-25 23:45:18 +0800

    Hari ke delapan puluh lima

---
+++ Day85.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main(String[] args) {
+       int[] angka = {10, 20, 30, 40};
+        
+       System.out.println("Isi array:");
+        for (int i = 0; i < angka.length; i++) {
+            System.out.println("Elemen ke-" + i + ": " + angka[i]);
+        }
+    }
+}
